5962eb083e24e8576c31e30e4f5da8fd9a36c417
Typhoon1551 <141539087+Typhoon1551@users.noreply.github.com>
2023-11-21 16:01:46 +0000

    edit

--- basic_proj/src/main.rs
+++ basic_proj/src/main.rs
@@ -1,14 +1,14 @@
 use rand;
 
 fn main() {
-    const SIZE_X:usize = 1000;
-    const SIZE_Y:usize = 1000;
+    const SIZE_X:usize = 10;
+    const SIZE_Y:usize = 10;
 
     let mut map:[[char; SIZE_X]; SIZE_Y] = [[' ';SIZE_X];SIZE_Y];
 
     for x in map.iter_mut() {
         for y in x.iter_mut() {
-            *y = if rand::random::<bool>() {'#'} else {' '};
+            *y = if rand::random::<bool>() {'@'} else {' '};
         }
     }
     
